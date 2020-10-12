5587ba8702b1c47cb240e14e7fbae02a8ee1ffb9
Samikshya <samikshyaaryal5@gmail.com>
2020-10-12 21:08:51 +0300

    Updates

---
+++ precache-manifest.0e40730e5efad74822db115dd20ccd00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966986f7201fdf329969f449f21eb53d",
+    "url": "/Cat-Facts/index.html"
+  },
+  {
+    "revision": "749decec1ae7e44a0efc",
+    "url": "/Cat-Facts/static/css/2.620ed6e8.chunk.css"
+  },
+  {
+    "revision": "5d6961856908ae81761d",
+    "url": "/Cat-Facts/static/css/main.dfc658e5.chunk.css"
+  },
+  {
+    "revision": "749decec1ae7e44a0efc",
+    "url": "/Cat-Facts/static/js/2.4845e9d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cat-Facts/static/js/2.4845e9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d6961856908ae81761d",
+    "url": "/Cat-Facts/static/js/main.e3141900.chunk.js"
+  },
+  {
+    "revision": "85e498de336af2b65a42",
+    "url": "/Cat-Facts/static/js/runtime-main.6f59e2d4.js"
+  },
+  {
+    "revision": "ba74bd7bb04b7b5584ca5a119a948cf9",
+    "url": "/Cat-Facts/static/media/cat.ba74bd7b.png"
+  }
+]);
